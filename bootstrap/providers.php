4fc70c82f3d263ef89bdd22324cead7d1c5972f4
Macbook Hafiz <fizlaw96@gmail.com>
2026-02-14 17:24:49 +0800

    Update providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    ...class_exists(Laravel\Sanctum\SanctumServiceProvider::class)
+        ? [Laravel\Sanctum\SanctumServiceProvider::class]
+        : [],
 ];
